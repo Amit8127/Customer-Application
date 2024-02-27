472e997f90ed2d01d852f9cecc96cf43aecc2fb3
Amit Kumar <amitmonu0000@gmail.com>
2024-02-27 15:25:32 +0530

    update url

--- src/services/serviceUtils.jsx
+++ src/services/serviceUtils.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getJwtToken } from "../auth/auth";
 
-export const BaseURL = "http://customer-app-env.eba-zk7p4xbp.ap-south-1.elasticbeanstalk.com/";
+export const BaseURL = "http://customer-app-env.eba-zk7p4xbp.ap-south-1.elasticbeanstalk.com";
 // export const BaseURL = "http://localhost:8080"; //for testing purposes
 
 export const auth = axios.create({
